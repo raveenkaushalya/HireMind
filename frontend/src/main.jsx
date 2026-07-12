181dbcf7c5b30eab9fbf9282cd5a25d6459fe076
Sarodye Weerasinghe <ssdweerasinghe@students.nsbm.ac.lk>
2026-07-12 21:35:17 +0530

    Recruiter portal updated.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function App() {
-  return <div>HireMind Frontend</div>;
-}
+import AppRoutes from './routes/AppRoutes';
+import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppRoutes />
   </React.StrictMode>
 );
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import RecruiterDashboard from '../pages/recruiter/RecruiterDashboard';
 
 export default function AppRoutes() {
-  return <div>Routes go here</div>;
+  return <RecruiterDashboard />;
 }
